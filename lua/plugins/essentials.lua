a79b40a5d1c4c36124fb5454acbd06270d6cc674
Lars Indreeide <lars.indreeide@remorarobotics.no>
2026-08-26 17:12:38 +0200

    First steps

---
+++ lua/plugins/essentials.lua
@@ -0,0 +1,28 @@
+return {
+{
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  },
+  keys = {
+    {
+      "<leader>?",
+      function()
+        require("which-key").show({ global = false })
+      end,
+      desc = "Buffer Local Keymaps (which-key)",
+    },
+  },
+},
+{
+    'nvim-telescope/telescope.nvim', version = '*',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        -- optional but recommended
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    }
+},
+}
